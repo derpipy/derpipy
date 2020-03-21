14023184194b737a856ea83c1c86f5714ad3b0c4
pingmader <pingmader@gmail.com>
2020-03-21 19:04:16 +0100

    Fix pony tag bug

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     keywords=(
         'derpibooru derpibooru.org python api wrapper my_little_pony mlp '
         'friendship_is_magic FiM fim earthpony unicorn pegasus' + reduce(
-            lambda a, x: a+' '+x, map(lambda x: x[0]+x[1], product(['pon', 'p0n'], ['', 'y', 'e', 'ies', 'es'])), ''
+            lambda a, x: a+' '+x, map(lambda x: x[0]+x[1], product(['pon', 'p0n'], ['', 'y', 'e', 'ies', 'es', 's']))
         )
     ),
     # You can just specify the packages manually here if your project is
