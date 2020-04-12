5613ad7c594a8fc76fa4fe12507cb78a2af041fc
luckydonald <mac2019.git@luckydonald.de>
2020-04-12 13:28:58 +0200

    Manually fixes to the API Docs in a single commit.

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     # What does your project relate to?
     keywords=(
         'derpibooru derpibooru.org python api wrapper my_little_pony mlp '
-        'friendship_is_magic FiM fim earthpony unicorn pegasus' + reduce(
+        'friendship_is_magic FiM fim earthpony unicorn pegasus ' + reduce(
             lambda a, x: a+' '+x, map(lambda x: x[0]+x[1], product(['pon', 'p0n'], ['', 'y', 'e', 'ies', 'es', 's']))
         )
     ),
