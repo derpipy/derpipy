c92706fe8bbfb17197e5c731458200db66821cd4
pingmader <42393017+pingmader@users.noreply.github.com>
2020-04-21 13:30:11 +0200

    Add luckydonald-utils to install_requires

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['iso8601'],
+    install_requires=['iso8601', 'luckydonald-utils'],
     # List additional groups of dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
     # $ pip install -e .[dev,test]
